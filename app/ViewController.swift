05751514b483c4cc5553e409efc51c4aff9a5f7c
Saif Rehman <s4saif.121@gmail.com>
2017-01-12 23:30:06 +0400

    init

---
+++ app/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by SaifRehman on 1/11/17.
+//  Copyright © 2017 SaifRehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var BackGroundBlur: UIImageView!
+    
+    @IBOutlet weak var roundedButton: UIButton!
+    @IBOutlet weak var roundedButton2: UIButton!
+    @IBOutlet weak var textArea1: UITextField!
+    @IBOutlet weak var textArea2: UITextField!
+    @IBAction func LoginButton(_ sender: UIButton) {
+        // for login navigation
+        if(true)
+        {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let resultViewController = storyBoard.instantiateViewController(withIdentifier: "StoryBoardID") as! UITabBarController
+            self.present(resultViewController, animated:true, completion:nil)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //styling adding blue and colors to appx
+        navigationController?.setNavigationBarHidden(navigationController?.isNavigationBarHidden == false, animated: true)
+        let blurEffect = UIBlurEffect(style:UIBlurEffectStyle.dark)
+        let blurView = UIVisualEffectView(effect:blurEffect)
+        blurView.frame = BackGroundBlur.bounds
+        BackGroundBlur.addSubview(blurView)
+        blurView.alpha = 0.3
+        UIView.animate(withDuration: 0.8) {
+            blurView.alpha = 1.0
+        }
+        roundedButton.backgroundColor = .clear
+        roundedButton.layer.cornerRadius = 5
+        roundedButton.layer.borderWidth = 1
+        roundedButton.layer.borderColor = UIColor.orange.cgColor
+        roundedButton2.backgroundColor = .clear
+        roundedButton2.layer.cornerRadius = 5
+        roundedButton2.layer.borderWidth = 1
+        roundedButton2.layer.borderColor = UIColor.red.cgColor
+        textArea1.backgroundColor = .black
+        textArea1.layer.cornerRadius = 5
+        textArea1.layer.borderWidth = 1
+        textArea1.layer.borderColor = UIColor.orange.cgColor
+        textArea2.backgroundColor = .black
+        textArea2.layer.cornerRadius = 5
+        textArea2.layer.borderWidth = 1
+        textArea2.layer.borderColor = UIColor.orange.cgColor
+
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+}
+
